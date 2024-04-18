bd46ede95dcb8a9f04627c5ab0795b6b4bb43be1
Tom Banister <tgbanister@gmail.com>
2024-04-18 18:11:36 +0100

    chrome good

--- src/Banner.js
+++ src/Banner.js
@@ -2,61 +2,71 @@ const logo = new URL('./assets/logo-banner.webp', import.meta.url)
 const room = new URL('./assets/stageupstairs.jpg', import.meta.url)
 const roomDown = new URL('./assets/stageroom.jpg', import.meta.url)
 
-// import styled, { keyframes } from 'styled-components'
-import { useState } from 'react'
+import styled, { keyframes } from 'styled-components'
+import { useState, useEffect } from 'react'
 
-// const changeBg = keyframes`
-//   0%,100%  {
-//     background-position: center;
-//   }
+const changeBg = keyframes`
+  0%,100%  {
+    background-position: center;
+  }
 
-//   25% {
-//     background-position: bottom right;
-//   }
+  24.9999% {
+    background-image: url(${room});
+  }
 
-//   50% {
-//     background-position: center;
-//   }
+  25% {
+    background-image: url(${roomDown});
+    background-position: bottom right;
+  }
 
-//   75% {
-//     background-position: top left;
-//   }
-// `
 
-// const BackgroundImageDiv = styled.div`
-//   background-image: url(${room});
-//   animation-name: ${changeBg};
-//   animation-duration: 40s;
-//   animation-iteration-count: infinite;
-//   animation-timing-function: linear;
-// `
+  50% {
+    background-position: center;
+  }
+
+  74.9999% {
+    background-image: url(${roomDown});
+  }
+  75% {
+    background-image: url(${room});
+    background-position: top left;
+  }
+`
+
+const BackgroundImageDiv = styled.div`
+  background-image: url(${room});
+  animation-name: ${changeBg};
+  animation-duration: 40s;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+`
 
 const Banner = () => {
   const [effectState, setEffectState] = useState(false)
 
-  // useEffect(() => {
-  //   if (!effectState) {
-  //     setEffectState(true)
-  //     var img = new Image()
-  //     img.src = roomDown.href
-  //     img.style = 'width: 1px; height: 1px;'
-  //     img.className = 'hide'
-  //     img.onload = function () {
-  //       document.body.appendChild(img)
+  useEffect(() => {
+    if (!effectState) {
+      setEffectState(true)
+      var img = new Image()
+      img.src = roomDown.href
+      img.style = 'width: 1px; height: 1px;'
+      img.className = 'hide'
+      img.onload = function () {
+        document.body.appendChild(img)
 
-  //       setTimeout(() => {
-  //         const imagesToHide = document.getElementsByClassName('hide')
+        setTimeout(() => {
+          const imagesToHide = document.getElementsByClassName('hide')
 
-  //         for (let item of imagesToHide) {
-  //           item.style = 'display: none'
-  //         }
-  //       }, 100)
-  //     }
-  //   }
-  // }, [])
+          for (let item of imagesToHide) {
+            item.style = 'display: none'
+          }
+        }, 100)
+      }
+    }
+  }, [])
 
   return (
-    <div className="banner-img">
+    <BackgroundImageDiv className="banner-img">
       <div style={{ backgroundColor: 'rgba(0,0,0,0.5)', width: '100%' }}>
         <img
           src={logo}
@@ -68,9 +78,8 @@ const Banner = () => {
             width: '200px',
           }}
         />
-        asdfasdf
       </div>
-    </div>
+    </BackgroundImageDiv>
   )
 }
 
