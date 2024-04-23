8771eba9e023f018d258631cbe6a9eb799d8d298
Tom Banister <tgbanister@gmail.com>
2024-04-23 09:45:37 +0100

    cleanup

--- src/EventsList.js
+++ src/EventsList.js
@@ -1,31 +1,25 @@
 import moment from 'moment'
 
-const EventsList = ({ events }) => {
-  console.log('events')
-  console.log(events)
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <div style={{ width: '100%' }}>
-        <table>
-          {events.map(
-            ({ name, description, startTime, endTime, image, alt }, index) => (
-              <tr>
-                <td>{moment(startTime).format('Do MMM')}</td>
-                <td>{name}</td>
-              </tr>
-            )
-          )}
-        </table>
-      </div>
-      <a
-        href="./#/events"
-        className="button"
-        style={{ marginTop: '10px', display: 'inline-block' }}
-      >
-        See Event Details
-      </a>
+const EventsList = ({ events }) => (
+  <div style={{ textAlign: 'center' }}>
+    <div style={{ width: '100%' }}>
+      <table>
+        {events.map(({ name, startTime }, index) => (
+          <tr key={index}>
+            <td>{moment(startTime).format('Do MMM')}</td>
+            <td>{name}</td>
+          </tr>
+        ))}
+      </table>
     </div>
-  )
-}
+    <a
+      href="./#/events"
+      className="button"
+      style={{ marginTop: '10px', display: 'inline-block' }}
+    >
+      See Event Details
+    </a>
+  </div>
+)
 
 export default EventsList
