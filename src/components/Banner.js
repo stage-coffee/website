6e277e9d7b598991f7f6ee652288494897f81362
Tom Banister <tgbanister@gmail.com>
2024-07-20 15:49:03 +0100

    job banner hide if no jobs

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,15 +2,19 @@ import useSWR from 'swr'
 const logo = new URL('../assets/logo-banner.webp', import.meta.url)
 
 import getBannersFromCMS from '../utils/getBannersFromCMS'
+import getJobsFromCMS from '../utils/getJobsFromCMS'
 
 import { useState, useEffect } from 'react'
 
 const Banner = () => {
+  const { data: jobsData, error } = useSWR('jobs', getJobsFromCMS)
   const { data } = useSWR('banners', getBannersFromCMS, {
     fallbackData: [''],
   })
   const [currentImageIndex, setCurrentImageIndex] = useState(0)
   const [updateImage, setUpdateImage] = useState(true)
+
+  const hasJobs = jobsData && jobsData.jobs.length > 0
 
   const changeBackgroundImage = () => {
     setCurrentImageIndex((prevIndex) => (prevIndex + 1) % data.length)
@@ -60,26 +64,28 @@ const Banner = () => {
           <img src={logo} alt="Stage Logo" />
         </a>
       </div>
-      <a
-        href="./#/jobs"
-        style={{
-          backgroundColor: 'rgb(255, 207, 98)',
-          display: 'block',
-          marginTop: 'auto',
-        }}
-      >
-        <h4
+      {hasJobs && (
+        <a
+          href="./#/jobs"
           style={{
-            fontWeight: 800,
-            paddingTop: '20px',
-            paddingBottom: '20px',
+            backgroundColor: 'rgb(255, 207, 98)',
             display: 'block',
-            margin: 0,
+            marginTop: 'auto',
           }}
         >
-          Join Our Team!
-        </h4>
-      </a>
+          <h4
+            style={{
+              fontWeight: 800,
+              paddingTop: '20px',
+              paddingBottom: '20px',
+              display: 'block',
+              margin: 0,
+            }}
+          >
+            Join Our Team!
+          </h4>
+        </a>
+      )}
     </div>
   )
 }
